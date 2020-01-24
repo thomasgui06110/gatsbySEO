c0bb00226fb378e66f88a1c729edbc4e7cf5cc06
thomasgui <guilhem.thomas@gmail.com>
2020-01-24 17:06:39 +0100

    optimisation des photos

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,7 +4,7 @@ import { Card, Button } from "react-bootstrap";
 const Post = (props) => (
   <div className="p-3">
     <Card>
-      <Card.Img variant="top" src={props.image} />
+      <Card.Img variant="top" src={props.image} alt={props.title} />
       <Card.Body>
         <Card.Title dangerouslySetInnerHTML={{__html: props.title}}/>
         <Card.Text dangerouslySetInnerHTML={{__html: props.excerpt}}/>
